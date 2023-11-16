0bdb88a27c2fa5fdd3e7e98114fb6a8ec0e50164
Quin <njibakeh3@gmail.com>
2023-11-15 21:51:56 -0600

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace PDFTranslatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.translateButton = new System.Windows.Forms.Button();
+            this.sourceLanguageComboBox = new System.Windows.Forms.ComboBox();
+            this.targetLanguageComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.selectPdf = new System.Windows.Forms.Button();
+            this.sourceLanguageLabel = new System.Windows.Forms.Label();
+            this.targetLanguageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // translateButton
+            // 
+            this.translateButton.Location = new System.Drawing.Point(317, 389);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(144, 49);
+            this.translateButton.TabIndex = 0;
+            this.translateButton.Text = "Translate";
+            this.translateButton.UseVisualStyleBackColor = true;
+            this.translateButton.Click += new System.EventHandler(this.translateButton_Click);
+            // 
+            // sourceLanguageComboBox
+            // 
+            this.sourceLanguageComboBox.FormattingEnabled = true;
+            this.sourceLanguageComboBox.Location = new System.Drawing.Point(142, 240);
+            this.sourceLanguageComboBox.Name = "sourceLanguageComboBox";
+            this.sourceLanguageComboBox.Size = new System.Drawing.Size(162, 21);
+            this.sourceLanguageComboBox.TabIndex = 1;
+            // 
+            // targetLanguageComboBox
+            // 
+            this.targetLanguageComboBox.FormattingEnabled = true;
+            this.targetLanguageComboBox.Location = new System.Drawing.Point(471, 240);
+            this.targetLanguageComboBox.Name = "targetLanguageComboBox";
+            this.targetLanguageComboBox.Size = new System.Drawing.Size(175, 21);
+            this.targetLanguageComboBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Source Language";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(466, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output Language";
+            // 
+            // selectPdf
+            // 
+            this.selectPdf.Location = new System.Drawing.Point(298, 61);
+            this.selectPdf.Name = "selectPdf";
+            this.selectPdf.Size = new System.Drawing.Size(183, 68);
+            this.selectPdf.TabIndex = 5;
+            this.selectPdf.Text = "Select PDF File";
+            this.selectPdf.UseVisualStyleBackColor = true;
+            this.selectPdf.Click += new System.EventHandler(this.selectPdf_Click);
+            // 
+            // sourceLanguageLabel
+            // 
+            this.sourceLanguageLabel.AutoSize = true;
+            this.sourceLanguageLabel.Location = new System.Drawing.Point(195, 264);
+            this.sourceLanguageLabel.Name = "sourceLanguageLabel";
+            this.sourceLanguageLabel.Size = new System.Drawing.Size(41, 13);
+            this.sourceLanguageLabel.TabIndex = 6;
+            this.sourceLanguageLabel.Text = "Default";
+            // 
+            // targetLanguageLabel
+            // 
+            this.targetLanguageLabel.AutoSize = true;
+            this.targetLanguageLabel.Location = new System.Drawing.Point(535, 268);
+            this.targetLanguageLabel.Name = "targetLanguageLabel";
+            this.targetLanguageLabel.Size = new System.Drawing.Size(41, 13);
+            this.targetLanguageLabel.TabIndex = 7;
+            this.targetLanguageLabel.Text = "Default";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.targetLanguageLabel);
+            this.Controls.Add(this.sourceLanguageLabel);
+            this.Controls.Add(this.selectPdf);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.targetLanguageComboBox);
+            this.Controls.Add(this.sourceLanguageComboBox);
+            this.Controls.Add(this.translateButton);
+            this.Name = "Form1";
+            this.Text = "Form1";            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button translateButton;
+        private System.Windows.Forms.ComboBox sourceLanguageComboBox;
+        private System.Windows.Forms.ComboBox targetLanguageComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button selectPdf;
+        private System.Windows.Forms.Label sourceLanguageLabel;
+        private System.Windows.Forms.Label targetLanguageLabel;
+    }
+
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupDocs.Translation.Cloud.SDK.NET.Model.Requests;
+using GroupDocs.Translation.Cloud.SDK.NET.Model;
+using GroupDocs.Translation.Cloud.SDK.NET;
+
+namespace PDFTranslatorApp
+{
+    public partial class Form1 : Form
+    {
+        private string pdfFilePath;
+        private string sourceLanguage;
+        private string targetLanguage;
+        private List<string> targetLanguages = new List<string>(); // Declare it at the class level
+
+
+        private Dictionary<string, string> languageNames = new Dictionary<string, string>
+        {
+            { "en", "English" },
+            { "es", "Spanish" },
+            { "fr", "French" },
+            { "de", "German" },
+            { "ur", "Urdu" },
+            { "ps", "Pashto" },
+            { "fa", "Farsi" }
+            // Add more languages as needed
+        };
+        private string outputPath = Directory.GetCurrentDirectory();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        public void Start()
+        {
+
+            // Convert the Dictionary to a List of key-value pairs
+            List<KeyValuePair<string, string>> languageList = languageNames.ToList();
+
+            // Set the ComboBox's DataSource, DisplayMember, and ValueMember
+            sourceLanguageComboBox.DataSource = languageList;
+            sourceLanguageComboBox.DisplayMember = "Value"; // Display the language name
+            sourceLanguageComboBox.ValueMember = "Key";     // Store the language code
+
+            // Repeat the same for targetLanguageComboBox if needed.
+            targetLanguageComboBox.DataSource = languageList.ToList();
+            targetLanguageComboBox.DisplayMember = "Value";
+            targetLanguageComboBox.ValueMember = "Key";
+
+            //Debug
+            /*
+                        Console.WriteLine("Source Language Items:");
+                        foreach (var item in sourceLanguageComboBox.Items)
+                        {
+                            Console.WriteLine($"Value: {((KeyValuePair<string, string>)item).Value}, Key: {((KeyValuePair<string, string>)item).Key}");
+                        }
+
+                        Console.WriteLine("Target Language Items:");
+                        foreach (var item in targetLanguageComboBox.Items)
+                        {
+                            Console.WriteLine($"Value: {((KeyValuePair<string, string>)item).Value}, Key: {((KeyValuePair<string, string>)item).Key}");
+                        }*/
+
+        }
+
+        private void selectPdf_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "PDF Files|*.pdf";
+                openFileDialog.Title = "Select a PDF File";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pdfFilePath = openFileDialog.FileName;
+                    selectPdf.Text = pdfFilePath;
+                }
+            }
+        }
+
+        private void sourceLanguageComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (sourceLanguageComboBox.SelectedItem == null)
+            {
+                var selectedItem = sourceLanguageComboBox.SelectedItem;
+                var itemKey = (KeyValuePair<string, string>)selectedItem;
+
+                Console.WriteLine($"Value: {((KeyValuePair<string, string>)itemKey).Value}, Key: {((KeyValuePair<string, string>)itemKey).Key}");
+
+                sourceLanguage = itemKey.Value;
+                sourceLanguageLabel.Text = sourceLanguage; // Update the source language label
+            }
+            else
+            {
+                MessageBox.Show("Error: The selected source language is null or not assigned.");
+            }
+        }
+
+        private void targetLanguageComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (targetLanguageComboBox.SelectedItem != null)
+            {
+                var selectedItem = (KeyValuePair<string, string>)targetLanguageComboBox.SelectedItem;
+                targetLanguage = selectedItem.Key;
+
+                Console.WriteLine($"Value: {((KeyValuePair<string, string>)selectedItem).Value}, Key: {((KeyValuePair<string, string>)selectedItem).Key}");
+
+                // Clear the list and add the selected target language
+                targetLanguages.Clear();
+                targetLanguages.Add(targetLanguage);
+
+                targetLanguageLabel.Text = targetLanguage; // Update the target language label
+            }
+            else
+            {
+                MessageBox.Show("Error: The selected target language is null or not assigned.");
+            }
+        }
+        private async void translateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var outputPath = "C:\\Users\\DropBearsLLC\\Desktop\\Python Translator\\C#\\translated.pdf"; // Define the output path for the translated PDF
+                await TranslatePdfFileAsync(pdfFilePath, outputPath);
+                MessageBox.Show("Translation completed. The translated PDF is saved as 'translated.pdf'.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        public async Task TranslatePdfFileAsync(string pdfFilePath, string outputPath)
+        {
+            // Initialize the GroupDocs Translation API configuration with your credentials
+
+            Configuration conf = new Configuration();
+            conf.ClientId = "3c3a5f75-09e4-4fa7-a982-7b9c8fbc531c";
+            conf.ClientSecret = "6f84693a57c90c77412320124d69d489";
+
+            TranslationApi api = new TranslationApi(conf);
+            FileApi fileApi = new FileApi(conf);
+
+            // Define local paths to upload and download files
+            string uploadPath = pdfFilePath; // You can use the provided pdfFilePath directly
+            string downloadPath = outputPath; // Specify the path where the translated PDF should be saved
+
+            // Read the PDF file into a stream
+            using (Stream stream = File.Open(uploadPath, FileMode.Open))
+            {
+                // Upload the PDF file to the GroupDocs Translation API
+                UploadFileRequest uploadRequest = new UploadFileRequest
+                {
+                    File = stream,
+                    path = Path.GetFileName(uploadPath), // Get just the file name
+                    storageName = sourceLanguage + targetLanguage + "" // Specify the storage name if needed
+                };
+
+                FilesUploadResult uploadResult = fileApi.UploadFile(uploadRequest);
+                Console.WriteLine("File uploaded: " + uploadResult.Uploaded[0]);
+
+                // Prepare the request to translate the uploaded PDF file
+                string name = Path.GetFileName(uploadPath);
+                string uploadName = sourceLanguage + targetLanguage + name; // You can set it as needed
+                string folder = ""; // Specify the folder name if needed
+                string pair = sourceLanguage + "-" + targetLanguage; // Replace with the desired language pair
+                string format = "pdf";
+                string outformat = "pdf";
+                string storage = "Temp Storage";
+                string saveFile = Path.GetFileName(outputPath); // Get the name of the output file
+                string savePath = sourceLanguage + targetLanguage + "translated";
+                bool isValid = false;
+                bool masters = false;
+                bool optimizePdf = false;
+                string origin = ".NET";
+                bool details = false;
+
+                Dictionary<int, List<List<string>>> frontMatterDict = new Dictionary<int, List<List<string>>>()
+        {
+            { 0, new List<List<string>>()
+                {
+                    new List<string>() { "title" },
+                    new List<string>() { "frontmatter", "title" },
+                    new List<string>() { "frontmatter", "description" }
+                }
+            }
+        };
+
+                Dictionary<int, List<string>> shortCodeDict = new Dictionary<int, List<string>>()
+        {
+            { 0, new List<string>() { "1", "3" } }
+        };
+
+                List<int> elements = new List<int>();
+
+                // Create the translation request
+                TranslateDocumentRequest request = api.CreateDocumentRequest(uploadName, folder, pair, format, outformat, storage, saveFile, savePath, masters, elements, origin, optimizePdf);
+
+                // Execute the translation task
+                TranslationResponse response = api.RunTranslationTask(request);
+                Console.WriteLine(response.Message);
+
+                // Download the translated file
+                DownloadFileRequest downloadRequest = new DownloadFileRequest
+                {
+                    storageName = storage,
+                    path = saveFile
+                };
+
+                Stream result = fileApi.DownloadFile(downloadRequest);
+                Console.WriteLine("Translated file downloaded");
+
+                // Save the translated file to the specified output path
+                using (FileStream file = new FileStream(downloadPath, FileMode.Create, FileAccess.Write))
+                {
+                    result.CopyTo(file);
+                }
+
+                Console.WriteLine("Translated file saved");
+            }
+        }
+    }
+}
+
+
